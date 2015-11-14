bda5c8cb50494a6f5510b466bd20de32b2263438
David Santiago Barrera <d4v1x.sb@gmail.com>
2015-11-14 14:52:27 +0000

    Created examples of the JUnit Tests.

---
+++ src/test/java/TestExample.java
@@ -0,0 +1,51 @@
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by davidsantiago on 14/11/15.
+ */
+public class TestExample {
+
+    private Collection collection;
+
+    @BeforeClass
+    public static void oneTimeSetUp() {
+        // one-time initialization code
+        System.out.println("@BeforeClass - oneTimeSetUp");
+    }
+
+    @AfterClass
+    public static void oneTimeTearDown() {
+        // one-time cleanup code
+        System.out.println("@AfterClass - oneTimeTearDown");
+    }
+
+    @Before
+    public void setUp() {
+        collection = new ArrayList();
+        System.out.println("@Before - setUp");
+    }
+
+    @After
+    public void tearDown() {
+        collection.clear();
+        System.out.println("@After - tearDown");
+    }
+
+    @Test
+    public void testEmptyCollection() {
+        assertTrue(collection.isEmpty());
+        System.out.println("@Test - testEmptyCollection");
+    }
+
+    @Test
+    public void testOneItemCollection() {
+        collection.add("itemA");
+        assertEquals(1, collection.size());
+        System.out.println("@Test - testOneItemCollection");
+    }
+}
