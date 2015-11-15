c1eabf95c2aaf4f2df79f235002b569a3ccbeac4
Ismael Ojeda Perez <iojedaperez@gmail.com>
2015-11-15 19:27:12 +0000

    ReadIni Exception handling implemented

--- src/main/java/es/david/aion/files/ReadIni.java
+++ src/main/java/es/david/aion/files/ReadIni.java
@@ -4,15 +4,22 @@ import org.ini4j.Wini;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
 
 /**
  * Created by Ismael Ojeda Perez on 15/11/2015.
  */
 public class ReadIni {
-    public void filterChat(String chat) throws URISyntaxException, IOException, InterruptedException {
-        String iniFileName = this.getClass().getResource(chat).toString();
-        Wini ini = new Wini(new File(iniFileName));
-        String logFileName = ini.get("LogFile", "name", String.class);
+    public static String load(String iniFile) {
+
+        String logDirName = "";
+        String logFileName = "";
+        try {
+            Wini ini = new Wini(new File(iniFile));
+            logDirName = ini.get("LogFile", "directory", String.class);
+            logFileName = ini.get("LogFile", "filename", String.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return (logDirName + "\\" + logFileName);
     }
 }
