2c5499ea693a71cd4b6ad18fe73f40197a908e2a
David Santiago Barrera <d4v1x.sb@gmail.com>
2015-11-15 14:28:52 +0000

    Create event class.

---
+++ src/main/java/es/david/aion/events/Event.java
@@ -0,0 +1,14 @@
+package es.david.aion.events;
+
+import java.util.TimerTask;
+
+/**
+ * Created by davidsantiago on 15/11/15.
+ */
+public class Event extends TimerTask{
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/es/david/aion/events/EventManager.java
@@ -0,0 +1,14 @@
+package es.david.aion.events;
+
+import java.util.Timer;
+
+/**
+ * Created by davidsantiago on 15/11/15.
+ */
+public class EventManager extends Timer {
+
+    public EventManager(String name) {
+        super(name);
+    }
+
+}
