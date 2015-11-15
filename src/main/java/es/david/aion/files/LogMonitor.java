c9b069264bebbf877eac673c6eedab2a40dbd69b
Ismael Ojeda Perez <iojedaperez@gmail.com>
2015-11-15 19:27:56 +0000

    LogMonitor Exception handling implemented

--- src/main/java/es/david/aion/files/LogMonitor.java
+++ src/main/java/es/david/aion/files/LogMonitor.java
@@ -4,30 +4,38 @@ package es.david.aion.files;
  * Created by Ismael Ojeda Perez on 15/11/2015.
  */
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
 
 /**
  * @author Crazy Natrim
  */
 public class LogMonitor
 {
-    public void filterChat(String chat) throws URISyntaxException, IOException, InterruptedException {
-        String filename = "/Users/davidsantiago/IdeaProjects/manakarna-timer/Chat.log";
-        FileReader theFile = new FileReader(filename);
-        BufferedReader br = new BufferedReader(theFile);
-        while (true) {
-            String line = br.readLine();
-            if (line == null) {
-                Thread.sleep(500);
-            } else {
-                // do something with line
+    public void filterChat(String logFile) {
 
-                if (line.contains("Seal")) {
-                    System.out.println(line);
+        try {
+            FileReader theFile = new FileReader(logFile);
+            BufferedReader br = new BufferedReader(theFile);
+            while (true) {
+                String line = br.readLine();
+                if (line == null) {
+                    Thread.sleep(500);
+                } else {
+                    // do something with line
+
+                    if (line.contains("Seal")) {
+                        System.out.println(line);
+                    }
                 }
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
